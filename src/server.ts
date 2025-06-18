55e2c7ad6d1b4a3bdee7662c1b6ce85f46316887
Roshanchau <roshanchau001@gmail.com>
2025-06-18 21:07:21 +0545

    cors

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173" , "https://forge-admin2.vercel.app"], credentials: true }));
 
 
 app.use(express.json());
