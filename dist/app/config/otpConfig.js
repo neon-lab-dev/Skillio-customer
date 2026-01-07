21c07b1b4c9cc0f40c857954f30cae7ecd814e3a
Roshanchau <roshanchau001@gmail.com>
2026-01-07 15:29:55 +0545

    Remove logs and dist from tracking

--- dist/app/config/otpConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.otpConfig = void 0;
-exports.otpConfig = {
-    otpLength: 6,
-    otpExpirationTime: 5 * 60 * 1000, // 5 minutes in milliseconds
-    testMode: true
-};
