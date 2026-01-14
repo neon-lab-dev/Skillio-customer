c86120beaba624a28ddce7bd83646cf13416ccc9
ritik-agrawal <ritikagrawal1697@gmail.com>
2026-01-14 21:41:04 +0530

    verification changes

--- src/app/modules/searchCriteria/search.criteria.plan.master.builder.ts
+++ src/app/modules/searchCriteria/search.criteria.plan.master.builder.ts
@@ -1,3 +1,4 @@
+import { PlanMasterStatus } from "../planMaster/enum/PlanMasterStatus";
 import { PlanMasterSearchCriteria } from "../planMaster/models/request/search.criteria.plan.master";
 
 
@@ -20,11 +21,23 @@ export class PlanMasterSearchCriteriaBuilder {
     public of ( query: Record<string, any>): PlanMasterSearchCriteriaBuilder {
         this.searchCriteria.setDefault(query);
         this.searchCriteria.ids = query.ids;
-        this.searchCriteria.codes = query.codes;
+        this.searchCriteria.code = query.code;
         this.searchCriteria.active = query.active ? Boolean(query.active) : undefined;
         this.searchCriteria.type = query.type;
         this.searchCriteria.profileVisibility = query.profileVisibility;
         this.searchCriteria.status = query.status;
+        this.searchCriteria.priceInPaiseMax = Number(query.priceInPaiseMax);
+        this.searchCriteria.priceInPaiseMin = Number(query.priceInPaiseMin);
+        this.searchCriteria.validityMin = Number(query.validityMin);
+        return this;
+    }
+
+    public userSearch( query: Record<string, any>): PlanMasterSearchCriteriaBuilder {
+        this.searchCriteria.setDefault(query);
+        this.searchCriteria.code = query.code;
+        this.searchCriteria.active = true;
+        this.searchCriteria.status = PlanMasterStatus.COMPLETE;
+        this.searchCriteria.type = query.type;
         this.searchCriteria.priceInPaiseMax = Number(query.priceInPaiseMax);
         this.searchCriteria.priceInPaiseMin = Number(query.priceInPaiseMin);
         this.searchCriteria.validityMin = Number(query.validityMin);
