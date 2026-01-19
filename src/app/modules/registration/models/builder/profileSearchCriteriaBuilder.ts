ceec264d96f91d24dec31f8a90f2be3cc67548d7
Roshanchau <roshanchau001@gmail.com>
2026-01-19 21:13:48 +0545

    fix!

--- src/app/modules/registration/models/builder/profileSearchCriteriaBuilder.ts
+++ src/app/modules/registration/models/builder/profileSearchCriteriaBuilder.ts
@@ -16,10 +16,6 @@ export class ProfileSearchCriteriaBuilder {
 
     public of( query: Record<string, any> ): ProfileSearchCriteriaBuilder {
         this.searchCriteria.setDefault(query);
-        this.searchCriteria.page = query.page ? Number(query.page) : 1;
-        this.searchCriteria.perPage = query.perPage ? Number(query.perPage) : 10;
-        this.searchCriteria.sortBy = query.sortBy ? query.sortBy : CREATED_AT;
-        this.searchCriteria.sortDirection = SearchCriteriaUtils.toSortDirection(query.sortDirection);
         this.searchCriteria.city= query.city;
         this.searchCriteria.country= query.country;
         this.searchCriteria.email= query.email;
