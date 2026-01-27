709d71990eeaba9c30251c559fedcd319cca3beb
Roshanchau <roshanchau001@gmail.com>
2026-01-27 13:12:11 +0545

    update : adds video file upload in cloudinary service

--- src/app/modules/document/services/cloudinaryServices.ts
+++ src/app/modules/document/services/cloudinaryServices.ts
@@ -1,15 +1,22 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiOptions } from "cloudinary";
 import { logger } from "../../../utils/logger";
 import AppError from "../../../errors/appError";
 
 class CloudinaryService {
   uploadFile = async (file: Express.Multer.File) => {
     try {
+
+      const isVideo = file.mimetype.startsWith("video/");
+      const isImage = file.mimetype.startsWith("image/");
+
+      const uploadOptions : UploadApiOptions = {
+        folder: "skilioDocument",
+        resource_type: isVideo ? "video" : isImage ? "image" : "auto",
+      };
+
       const res = await cloudinary.uploader.upload(
         `data:${file.mimetype};base64,${file.buffer.toString("base64")}`,
-        {
-          folder: "skilioDocument",
-        }
+        uploadOptions
       );
 
       return res;
