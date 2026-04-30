3392aabe33239b29bd5c11f226a9a1a8d633a0ba
Roshanchau <roshanchau001@gmail.com>
2026-04-30 17:11:56 +0545

    update category and subCategory api

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 import { CreateCategoryApi } from "./api/createCategoryApi";
 import { FetchCategoriesApi } from "./api/fetchCategoriesApi";
 import { verifyToken } from "../../middlewares/requireAuth";
-import authorizeRole from "../../middlewares/authorizeRole";
-import { roles } from "../registration/enums/registrationEnum";
 import { asyncApiHandler } from "@neon-lab-dev/platform";
 
 const router= Router();
@@ -13,7 +11,6 @@ const fetchCategoriesApi = new FetchCategoriesApi();
 router.post(
     "/",
     verifyToken,
-    authorizeRole.validateRole(roles.ADMIN),
     asyncApiHandler(createCategoryApi)
 )
 
